332a9e5eabeb853db66315700c283a79f2a01bcc
Egor Aristov <ef@efprojects.com>
2025-10-23 16:44:50 +0300

    imaginary example

--- example/ts/index.ts
+++ example/ts/index.ts
@@ -1,15 +1,36 @@
+import {KittenIPC} from '../../lib/ts/lib.js';
+import GoIpcApi from './goapi.gen.ts';
+
 /**
  * @kittenipc api
  */
 class TsIpcApi {
     Div(a: number, b: number): number {
+        if (b === 0) {
+            throw new Error('division by zero');
+        }
         return a / b;
     }
 }
 
+async function main() {
+    const localApi = new TsIpcApi();
+    const ipc = new KittenIPC(localApi);
+    const goApi = new GoIpcApi(ipc);
 
-function main() {
+    await ipc.start();
 
+    console.log(`12/3=${await goApi.Div(12, 3)}`);
+
+    try {
+        await goApi.Div(10, 0);
+    } catch (e) {
+        console.trace(e);
+    }
+
+    await ipc.wait();
 }
 
-main()
+main().catch(e => {
+    console.trace(e);
+});
