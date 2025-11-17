07c8367abb8476aac1cc5ebb0c0424deed7e1f6e
Egor Aristov <ef@efprojects.com>
2025-11-17 09:33:36 +0300

    move testdata

--- lib/ts/src/lib.test.ts
+++ lib/ts/src/lib.test.ts
@@ -2,13 +2,13 @@ import {test} from 'vitest';
 import {ParentIPC} from './lib.js';
 
 test('test connection timeout', async ({expect}) => {
-    const parentIpc = new ParentIPC('testdata/sleep15.sh', []);
+    const parentIpc = new ParentIPC('../testdata/sleep15.sh', []);
     await parentIpc.start();
     await expect(parentIpc.wait()).rejects.toThrowError('timed out');
 });
 
 test('test process stop before connection accept', async ({expect}) => {
-    const parentIpc = new ParentIPC('testdata/sleep3.sh', []);
+    const parentIpc = new ParentIPC('../testdata/sleep3.sh', []);
     await parentIpc.start();
     await expect(parentIpc.wait()).rejects.toThrowError('command exited before connection established');
 });
