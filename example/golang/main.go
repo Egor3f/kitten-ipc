423a809e7e432532b48d54e3304f776ddfddbe1e
Egor Aristov <ef@efprojects.com>
2026-03-28 17:21:26 +0300

    disable debug

--- example/golang/main.go
+++ example/golang/main.go
@@ -47,7 +47,7 @@ func main() {
 
 	cmd := exec.Command("node", path.Join(cwd, "ts/dist/index.js"))
 
-	ipc, err := kittenipc.NewParent(cmd, &kittenipc.Options{DebugMessages: true}, &localApi)
+	ipc, err := kittenipc.NewParent(cmd, &kittenipc.Options{DebugMessages: false}, &localApi)
 	if err != nil {
 		log.Panic(err)
 	}
--- example/ts/src/index.ts
+++ example/ts/src/index.ts
@@ -28,7 +28,7 @@ class TsIpcApi {
 
 async function main() {
     const localApi = new TsIpcApi();
-    const ipc = new ChildIPC({debugMessages: true}, localApi);
+    const ipc = new ChildIPC({debugMessages: false}, localApi);
     const remoteApi = new GoIpcApi(ipc);
 
     await ipc.start();
